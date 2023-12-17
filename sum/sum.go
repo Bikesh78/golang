41a73a53651aa8ed4a6eedaa42512ae356536dcc
bikesh <bikeshrai78@gmail.com>
2023-12-17 17:23:13 +0545

    Changes

--- sum/sum.go
+++ sum/sum.go
@@ -7,3 +7,33 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	// lengthOfNumbers := len(numbersToSum)
+	// sums:= make([]int,lengthOfNumbers)
+	//
+	// for i, number := range numbersToSum{
+	//   sums[i] = Sum(number)
+	// }
+	// return sums
+
+	var sums []int
+	for _, number := range numbersToSum {
+		sums = append(sums, Sum(number))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, number := range numbersToSum {
+		if len(number) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := number[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
