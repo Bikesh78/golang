f04e882d11bbb4b8a6fdb089d1084b395332ae9e
bikesh <bikeshrai78@gmail.com>
2024-01-16 10:56:33 +0545

    Finish mocking lesson

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -11,26 +11,27 @@ type Sleeper interface {
 	Sleep()
 }
 
-
 const finalWord = "Go!"
 const countdownStart = 3
 
 func Countdown(writer io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(writer, i)
-		// time.Sleep(1 * time.Second)
 		sleeper.Sleep()
 	}
 	fmt.Fprint(writer, finalWord)
 }
 
-type DefaultSleeper struct{}
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
 
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
