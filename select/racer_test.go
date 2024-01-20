55877b48fde776118a4d21c4e0d97a354eb9cc28
bikesh <bikeshrai78@gmail.com>
2024-01-20 16:12:43 +0545

    Select version 2

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,10 +1,21 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.dev"
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
@@ -13,4 +24,12 @@ func TestRacer(t *testing.T) {
 		t.Errorf("got %q want %q", got, want)
 	}
 
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+	return server
 }
